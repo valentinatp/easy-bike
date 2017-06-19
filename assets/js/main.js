b4f594479206e94aa67f590a54214ab0771c90b0
valentinatp <valentina.toledoporma@gmail.com>
2017-06-19 12:20:09 -0400

    Creo la funcion initmap

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-main.js
+function initmap(){
+	var map = new google.maps.Map(document.getElementById("map"),{
+		zoom: 5,
+		center: {lat: -91191427, lng: -77.0349046},
+		mapTypeControl:false;
+		zoomControl: false,
+		streetViewControl: false
+	});
+}
